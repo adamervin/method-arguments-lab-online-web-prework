a77e3bc5edfaac4dfe9c633a6d3bec7edf2a168c
adam <adamjervin@gmail.com>
2019-05-19 05:01:38 +0000

    Automatically backed up by Learn

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -1,8 +1,7 @@
-require "spec_helper"
 def introduction(name)
-  puts "Hi, my name is #
-  {name}."
+  puts "Hi, my name is #{name}."
 end
+require "spec_helper"
 describe "#introduction" do 
   it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do 
     expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
