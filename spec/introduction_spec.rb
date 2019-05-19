5d0f440e2e8d1508230bef06c0d71f87f2e51d4d
adam <adamjervin@gmail.com>
2019-05-19 04:52:09 +0000

    Automatically backed up by Learn

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -1,5 +1,5 @@
 require "spec_helper"
-def introduction
+def introduction(name)
 end
 describe "#introduction" do 
   it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do 
